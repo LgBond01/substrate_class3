7d21b6adb4bc1497a613da2489ba1e89fb0b4fde
LgBond01 <86766708+LgBond01@users.noreply.github.com>
2023-12-07 23:11:53 +0800

    Add files via upload

---
+++ 第六课作业/polkadot/astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import qwikdev from '@qwikdev/astro';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations:  [qwikdev()],
+});
