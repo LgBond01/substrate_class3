84870ca96d49d51d10784c20de6fc2ec9a4ff88b
LgBond01 <86766708+LgBond01@users.noreply.github.com>
2023-12-10 01:09:02 +0800

    Delete 第六课作业/polkadot directory

--- 第六课作业/polkadot/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import qwikdev from '@qwikdev/astro';
-
-// https://astro.build/config
-export default defineConfig({
-    integrations:  [qwikdev()],
-});
